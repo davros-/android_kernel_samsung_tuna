e68f2b8d724bd6e86dd4409a75f2884d6692b16c
buckmarble <forrestmarble@gmail.com>
2012-11-02 16:05:26 -0400

    buck and Ezekeel

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -427,6 +427,18 @@ struct freq_attr omap_cpufreq_attr_screen_off_freq = {
 	.store = store_screen_off_freq,
 };
 
+#if defined(CONFIG_OMAP_SCALING_FREQS) || defined(CONFIG_OMAP_SCREENOFF_MAXFREQ)
+static struct freq_attr *omap_cpufreq_attr[] = {
+#ifdef CONFIG_OMAP_SCALING_FREQS
+          &cpufreq_freq_attr_scaling_available_freqs,
+#endif
+#ifdef CONFIG_OMAP_SCREENOFF_MAXFREQ
+          &omap_cpufreq_attr_screen_off_freq,
+#endif
+NULL,
+};
+#endif
+
 /*
  * Variable GPU OC - sysfs interface for cycling through different GPU top speeds
  * Author: imoseyon@gmail.com
